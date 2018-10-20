ebf14b9d09db260ca8a6e0da0fc177ab413d1ae4
Bock  Christian <chrs.bock@gmail.com>
2018-10-20 21:31:19 +0200

    Found a little nicer way to find preprocessing function. Fixes #4

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -5,30 +5,30 @@ class Preprocessing():
 
 	def __init__(self, fhir_class: Union[Patient]):
 		if type(fhir_class) != type:
-			raise ValueError("{} is not a type but a {}. Please use the desired class you want to process (e.g. Patient)".format(fhir_class, type(fhir_class)))
-
-
-		### This is currently an ugly way of storing which classes and attibutes can be preprocessed
-		self.preprocessing_dict = { Patient: { 'address': self.preprocess_address } }
-
-		if fhir_class not in self.preprocessing_dict.keys():
-			raise NotImplementedError("Sorry, we don't know how to preprocess objects of type {}.".format(fhir_class))
+			raise ValueError("""{} is not a type but a {}. 
+				Please use the desired class you want to process (e.g. Patient)""".format(fhir_class, type(fhir_class)))
 		else:
 			self.fhir_class = fhir_class
-			self.class_preprocessing_functions = self.preprocessing_dict[fhir_class]
 
-	def can_preprocess(self, attr: str):
-		if attr not in self.class_preprocessing_functions:
-			raise NotImplementedError("Sorry, we don't know how to preprocess the attribute {} of {}.".format(attr, self.fhir_class))
+	def get_preprocessing_func(self, attr: str):
+		needed_module_name = self.fhir_class.__module__.lower()
+		needed_func_name = ('_'.join(['preprocess_{}'.format(needed_module_name), attr]))
+		if needed_func_name not in dir(self):
+			available_attributes = [x.split('_')[2] for x in dir(self) if needed_module_name in x ]
+			if len(available_attributes) > 0:
+				raise NotImplementedError("""Sorry, we don't know how to preprocess the attribute {}. 
+					Available attributes are {}.""".format(attr, ', '.join(available_attributes)))
+			else:
+				raise NotImplementedError("Sorry, we don't know how to preprocess the attribute {}.".format(attr))
 		else:
-			return True
+			return needed_func_name
 
 	
-	def preprocess_address(self, X: List[Union[Patient]]):
+	def preprocess_patient_address(self, X: List[Union[Patient]]):
 		return ['test']
 
 
 	def preprocess_on(self, X: List[Union[Patient]], attr: str):
-		if self.can_preprocess(attr):
-			return self.preprocessing_dict[self.fhir_class][attr](X)
-			pass
+		preprocessing_func = self.get_preprocessing_func(attr)
+		if preprocessing_func:
+			return getattr(self, preprocessing_func)(X)
