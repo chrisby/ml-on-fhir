8d9be82b4c7bb8d78994022d9cfe7bc9106ac4a9
Bock  Christian <chrs.bock@gmail.com>
2018-10-28 11:21:44 +0100

    Removed obsolete Classifier class

--- src/ml_on_fhir.py
+++ src/ml_on_fhir.py
@@ -149,18 +149,3 @@ class MLOnFHIR(BaseEstimator, ClassifierMixin):
 
     def score(self, X, y=None):
         return self.clf.score(X, y)
-
-
-class Classifier():
-
-    def __init__(self, fhir_class: Union[Patient]):
-        self.preprocessor = Preprocessing(fhir_class)
-
-    def classify_on(self, X: List[Union[Patient]], feature_attrs: List[str], label_attrs: List[str], how: str='knn'):
-        try:
-            X_preprocessed = self.preprocessor.preprocess_on(X, feature_attrs)
-            y = self.preprocessor.preprocess_on(X, label_attrs)
-
-            return X_preprocessed, y
-        except:
-            raise
