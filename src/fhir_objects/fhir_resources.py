ca24741021680eeae7defa137ec9c1514499de36
Bock  Christian <chrs.bock@gmail.com>
2018-11-04 15:56:16 +0100

    Added fhir resources for new fhir objects

--- src/fhir_objects/fhir_resources.py
+++ src/fhir_objects/fhir_resources.py
@@ -1,5 +1,14 @@
-patient_resources = ['identifier', 'active', 'gender', 'name',
+patient_resources = ['identifier', 'resourceType', 'active', 'gender', 'name',
                      'birthDate', 'deceased', 'deceased', 'maritalStatus']
-condition_resources = ['identifier', 'clinicalStatus', 'verificationStatus', 'category',
-                     'severity', 'code', 'subject', 'onset']                     
+
+condition_resources = ['identifier', 'resourceType', 'clinicalStatus', 'verificationStatus', 'category',
+                     'severity', 'code', 'subject', 'onset']
+
+observation_resources = ['identifier', 'resourceType', 'status', 'category', 'code', 'subject', 'context',
+                      'issued', 'performer', 'value', 'interpretation', 'comment', 'bodySite',
+                      'method', 'referenceRange', 'component']  
+
+procedure_resources = ['identifier', 'resourceType', 'subject', 'status', 'notDone', 'notDoneReason', 'category', 'code',
+                       'performed', 'reasonCode', 'bodySite', 'outcome', 'report', 'performedPeriod']
+
 date_format = '%Y-%m-%d'
