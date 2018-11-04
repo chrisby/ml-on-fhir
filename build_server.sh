e4aa8da5ed19149e890e34931c4146861a599759
Bock  Christian <chrs.bock@gmail.com>
2018-11-04 16:07:43 +0100

    Added run_server script

--- build_server.sh
+++ build_server.sh
@@ -1,2 +1,1 @@
 docker build -t smartonfhir/hapi:r3-synthea --build-arg FHIR=dstu3 --build-arg DATA=./databases/r3/synthea --build-arg CLI_OPTS=-Xmx1024m --squash .
-
---
+++ run_server.sh
@@ -0,0 +1,1 @@
+docker run -it -p 8080:8080 smartonfhir/hapi:r3-synthea
