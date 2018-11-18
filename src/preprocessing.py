1c9fbed78e3f57362a5542500c4aad6d24315fd5
Bock  Christian <chrs.bock@gmail.com>
2018-11-18 16:53:04 +0100

    Simplified way of registering Patient Processors for Observation Processor. They now get automatically generated. Fixes #18

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -15,16 +15,16 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.utils.validation import column_or_1d
 
 
-
 def register_preprocessor(processor_class: BaseEstimator):
     """
     Registers a new preprocessing class with MLOnFhir.
     Preprocessors with the same class name will be overwritten
 
     Args:
-        processor_class (BaseEstimator): Class that implements sklearn.base.BaseEstimator interface 
+        processor_class (BaseEstimator): Class that implements sklearn.base.BaseEstimator interface
     """
-    preprocessing_module = import_module(get_observation_preprocessors.__module__)
+    preprocessing_module = import_module(
+        get_observation_preprocessors.__module__)
     preprocessing_class_name = processor_class.__name__
     if hasattr(preprocessing_module, preprocessing_class_name):
         logging.warning("Preprocessor {} already exists. Will be overridden.".format(
@@ -32,9 +32,27 @@ def register_preprocessor(processor_class: BaseEstimator):
 
     setattr(preprocessing_module, preprocessing_class_name, processor_class)
 
+    # If a non-patient processor has been registered, we need to add one.
+    if preprocessing_class_name[:len("Patient")] != "Patient":
+        logging.info("Adding Patient Processor for {}".format(
+            preprocessing_class_name))
+        tmp_obj = processor_class()
+        if not hasattr(tmp_obj, 'patient_attribute_name'):
+            del tmp_obj
+            raise ValueError(
+                "Class {} does not have a patient_attribute_name attribute. Will not generate Patient Processor")
+        else:
+            new_name = 'Patient{}Processor'.format(
+                tmp_obj.patient_attribute_name)
+            logging.info(
+                "Name of patient processor will be {}".format(new_name))
+            new_class = PatientProcessorFactory(new_name)
+            register_preprocessor(new_class)
+
 
 def get_observation_preprocessors():
-    preprocessing_module = import_module(get_observation_preprocessors.__module__)
+    preprocessing_module = import_module(
+        get_observation_preprocessors.__module__)
     preprocessors = []
     for member_name, member in preprocessing_module.__dict__.items():
         if callable(member):
@@ -46,6 +64,31 @@ def get_observation_preprocessors():
         logging.warning(
             "Could not find any Observation preprocessors. Usage might be limited.")
     return preprocessors
+
+
+class PatientProcessorBaseClass(BaseEstimator):
+    """
+    Base class that is used for the generation of Patient Processors 
+    """
+
+    def transform(self, X, **transform_params):
+        return X.astype(float)
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+
+def PatientProcessorFactory(class_name: str, base_class=PatientProcessorBaseClass):
+    """
+    Helper class to generate Patient Processor classes with specific names
+    """
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        base_class.__init__(self)
+    new_class = type(class_name, (base_class,), {"__init__": __init__})
+    return new_class
 
 
 class FHIRLabelEncoder(BaseEstimator):
@@ -63,7 +106,7 @@ class FHIRLabelEncoder(BaseEstimator):
         return self
 
 
-class PatientGenderProcessor(FHIRLabelEncoder):
+class PatientgenderProcessor(FHIRLabelEncoder):
     """
     Encodes gender gender into integer values
     """
@@ -75,9 +118,9 @@ class PatientGenderProcessor(FHIRLabelEncoder):
         return super().fit(X, y, **fit_params)
 
 
-class PatientBirthdateProcessor(BaseEstimator):
+class PatientbirthDateProcessor(BaseEstimator):
     """
-    Calculates the age to use birthdate as a feature 
+    Calculates the age to use birthdate as a feature
     """
 
     def transform(self, X, **transform_params):
