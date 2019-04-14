084d9802b2cf70d974155932b860ca083a32894d
Bock  Christian <chrs.bock@gmail.com>
2019-04-14 17:32:03 +0200

    Added markdown support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,10 +30,9 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
 
-# Add any paths that contain templates here, relative to this directory.
+ Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
